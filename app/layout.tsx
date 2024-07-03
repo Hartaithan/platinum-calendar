3b7cfaa853945278c99df3afd30686034af8e671
Hartaithan <hartaithan@gmail.com>
2024-07-03 16:00:25 +0900

    install eslint and prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { FC, PropsWithChildren } from "react";
+import type { FC, PropsWithChildren } from "react";
 import "./globals.css";
 
 const font = Inter({ subsets: ["latin"], fallback: ["Arial"] });
 
 export const metadata: Metadata = {
-  title: "Trophy Hunt Overlay",
-  description: "Overlay for Trophy Hunt streamers",
+  title: "Platinum Calendar",
+  description: "Platinum Calendar Generator",
 };
 
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
