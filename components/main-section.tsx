ee877823b2871680ecd011d81a59d5d24861fefe
Hartaithan <hartaithan@gmail.com>
2024-07-08 11:12:47 +0900

    parse profile stats

--- components/main-section.tsx
+++ components/main-section.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { examples } from "@/examples";
+import { getProfile } from "@/utils/profile";
 import type { FormEventHandler } from "react";
 import { useCallback, type FC } from "react";
 
@@ -14,16 +16,20 @@ const MainSection: FC = () => {
     const id = target.id.value;
     const form = { id };
     console.info("form :>> ", form);
+    const res = getProfile(examples["player-profile"]);
+    console.info("res :>> ", res);
   }, []);
 
   return (
-    <form className="w-80" onSubmit={handleSubmit}>
-      <input
-        name="id"
-        className="block w-full text-sm rounded-md py-2 pl-4 ring-1 ring-inset bg-zinc-900 ring-zinc-800 placeholder:text-zinc-600 focus:ring-1 focus:ring-inset focus:ring-zinc-600"
-        placeholder="Enter your PSN ID"
-      />
-    </form>
+    <div className="flex flex-col w-[500px]">
+      <form className="w-full" onSubmit={handleSubmit}>
+        <input
+          name="id"
+          className="block w-full text-sm rounded-md py-2 pl-3 ring-1 ring-inset bg-zinc-900 ring-zinc-800 placeholder:text-zinc-600 focus:ring-1 focus:ring-inset focus:ring-zinc-600"
+          placeholder="Enter your PSN ID"
+        />
+      </form>
+    </div>
   );
 };
 
--- utils/profile.ts
+++ utils/profile.ts
@@ -1,22 +1,86 @@
 import type { ProfileResponse } from "@/models/profile";
+import type { AnyNode, CheerioAPI } from "cheerio";
 import { load } from "cheerio";
 
 const select = {
   name: "span.username",
   avatar: "div.avatar img",
   level: "div.level-box span",
+  stats: "div.stats > span.stat",
 };
 
 const notFound = "Not Found";
+
+const statsMapped: Record<number, string> = {
+  0: "games_played",
+  1: "completed_games",
+  2: "completion",
+  3: "unearned_trophies",
+  4: "trophies_per_day",
+  5: "views",
+  6: "world_rank",
+  7: "country_rank",
+};
+
+const cleanStat = (cheerio: CheerioAPI, node: AnyNode): string => {
+  const element = cheerio(node).clone();
+  const links = element.has("a");
+  if (links.length > 0) {
+    const link = element.find("a");
+    return link.children().remove().end().text().trim();
+  } else {
+    return element.children().remove().end().text().trim();
+  }
+};
+
+const getStats = (cheerio: CheerioAPI): Record<string, string> => {
+  const elements = cheerio(select.stats);
+  const stats: Record<string, string> = {
+    games_played: notFound,
+    completed_games: notFound,
+    completion: notFound,
+    unearned_trophies: notFound,
+    trophies_per_day: notFound,
+    views: notFound,
+    world_rank: notFound,
+    country_rank: notFound,
+  };
+  elements.each((i, stat) => {
+    const key = statsMapped[i];
+    stats[key] = cleanStat(cheerio, stat);
+  });
+  return stats;
+};
 
 export const getProfile = (content: string): Partial<ProfileResponse> => {
   const cheerio = load(content);
 
   const name = cheerio(select.name).text() || notFound;
-
   const avatar_url = cheerio(select.avatar).first().attr("src") || notFound;
-
   const level = cheerio(select.level).first().text() || notFound;
 
-  return { name, avatar_url, level };
+  const {
+    games_played,
+    completed_games,
+    completion,
+    unearned_trophies,
+    trophies_per_day,
+    views,
+    world_rank,
+    country_rank,
+  } = getStats(cheerio);
+
+  return {
+    name,
+    avatar_url,
+    level,
+    games_played,
+    completed_games,
+    completion,
+    unearned_trophies,
+    trophies_per_day,
+    views,
+    world_rank,
+    country_rank,
+  };
 };
