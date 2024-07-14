f958195074184b605f89846c13e9fa7dc5f93a5a
Hartaithan <hartaithan@gmail.com>
2024-07-14 14:30:11 +0900

    load profile and platinums data

--- components/main-section.tsx
+++ components/main-section.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { examples } from "@/examples";
-import { getPlatinums } from "@/utils/trophies";
+import type { PlatinumsResponse } from "@/models/trophy";
+import { useData } from "@/providers/DataProvider";
+import { fetchPlatinums, fetchProfile } from "@/utils/fetch";
 import type { FormEventHandler } from "react";
 import { useCallback, type FC } from "react";
 
@@ -10,25 +11,62 @@ interface Form {
 }
 
 const MainSection: FC = () => {
-  const handleSubmit: FormEventHandler<HTMLFormElement> = useCallback((e) => {
-    e.preventDefault();
-    const target = e.target as typeof e.target & Form;
-    const id = target.id.value;
-    const form = { id };
-    console.info("form :>> ", form);
-    const res = getPlatinums(examples["player-platinums"]);
-    console.info("res :>> ", res);
-  }, []);
+  const { profile, platinums, setProfile, setPlatinums } = useData();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = useCallback(
+    async (e) => {
+      e.preventDefault();
+      const target = e.target as typeof e.target & Form;
+      const id = target.id.value;
+      try {
+        const profile = await fetchProfile(id);
+        if (!profile) {
+          // TODO: handle errors
+          return;
+        }
+        setProfile(profile);
+        const page_count = Math.ceil(profile.counts.platinum / 50);
+        const requests: Promise<PlatinumsResponse | null>[] = [];
+        for (let i = 1; i <= page_count; i++) {
+          requests.push(fetchPlatinums(id, i));
+        }
+        const responses = await Promise.all(requests);
+        for (const response of responses) {
+          const list = response?.list ?? [];
+          setPlatinums((prev) => {
+            const arr = prev ?? [];
+            return [...arr, ...list];
+          });
+        }
+      } catch (error) {
+        // TODO: handle errors
+        console.info("error", error);
+      }
+    },
+    [setProfile, setPlatinums],
+  );
 
   return (
-    <div className="flex flex-col w-[500px]">
-      <form className="w-full" onSubmit={handleSubmit}>
+    <div className="flex flex-col justify-center items-center">
+      <form className="w-[500px]" onSubmit={handleSubmit}>
         <input
           name="id"
-          className="block w-full text-sm rounded-md py-2 pl-3 ring-1 ring-inset bg-zinc-900 ring-zinc-800 placeholder:text-zinc-600 focus:ring-1 focus:ring-inset focus:ring-zinc-600"
+          className="w-[500px] block text-sm rounded-md py-2 pl-3 ring-1 ring-inset bg-zinc-900 ring-zinc-800 placeholder:text-zinc-600 focus:ring-1 focus:ring-inset focus:ring-zinc-600"
           placeholder="Enter your PSN ID"
         />
       </form>
+      <div className="flex gap-6 mt-6">
+        <div className="w-[500px] overflow-auto max-h-80 mt-6">
+          <pre className="text-[9px] whitespace-pre-wrap">
+            {JSON.stringify(profile, null, 2)}
+          </pre>
+        </div>
+        <div className="w-[500px] overflow-auto max-h-80 mt-6">
+          <pre className="text-[9px] whitespace-pre-wrap">
+            {JSON.stringify(platinums, null, 2)}
+          </pre>
+        </div>
+      </div>
     </div>
   );
 };
