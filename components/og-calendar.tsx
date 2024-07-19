f02ac097dc5d3ad3e764d287a53277e5a9e8a42c
Hartaithan <hartaithan@gmail.com>
2024-07-19 11:18:03 +0900

    show marks on calendar

--- components/og-calendar.tsx
+++ components/og-calendar.tsx
@@ -1,4 +1,6 @@
-import { months } from "@/constants/calendar";
+import { monthIndex, months } from "@/constants/calendar";
+import type { Platinum } from "@/models/trophy";
+import { useData } from "@/providers/DataProvider";
 import { createArray } from "@/utils/array";
 import { memo, type FC } from "react";
 
@@ -8,14 +10,34 @@ interface MonthProps {
 }
 
 interface DayProps {
+  month: string;
   day: number;
 }
 
-const Day: FC<DayProps> = memo((props) => {
-  const { day } = props;
+interface MarkProps {
+  list: Platinum[] | null;
+}
+
+const Mark: FC<MarkProps> = (props) => {
+  const { list } = props;
+  if (!list) return null;
+  if (list.length === 0) return null;
   return (
-    <div className="day size-8 flex outline outline-1 outline-white justify-center items-center">
+    <div className="absolute bg-red-600 size-full rounded-full flex justify-center items-center">
+      <p className="text-sm">{list?.length}</p>
+    </div>
+  );
+};
+
+const Day: FC<DayProps> = memo((props) => {
+  const { month, day } = props;
+  const { platinums } = useData();
+  const key = `${day}.${monthIndex[month]}`;
+  const list = platinums ? platinums[key] : null;
+  return (
+    <div className="day size-8 flex outline outline-1 outline-white justify-center items-center relative">
       {day}
+      <Mark list={list} />
     </div>
   );
 });
@@ -29,8 +51,8 @@ const Month: FC<MonthProps> = memo((props) => {
         {month}
       </div>
       <div className="grid grid-cols-7 gap-[1px]">
-        {days.map((i) => (
-          <Day key={`month-${i}`} day={i} />
+        {days.map((day) => (
+          <Day key={`month-${day}`} month={month} day={day} />
         ))}
       </div>
     </div>
--- constants/calendar.ts
+++ constants/calendar.ts
@@ -12,3 +12,18 @@ export const months: Record<string, number> = {
   November: 30,
   December: 31,
 };
+
+export const monthIndex: Record<string, number> = {
+  January: 1,
+  February: 2,
+  March: 3,
+  April: 4,
+  May: 5,
+  June: 6,
+  July: 7,
+  August: 8,
+  September: 9,
+  October: 10,
+  November: 11,
+  December: 12,
+};
