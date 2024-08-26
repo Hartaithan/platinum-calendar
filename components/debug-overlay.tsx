42903d044962bfceb2734f7f199a9008f864ea2e
Hartaithan <hartaithan@gmail.com>
2024-08-26 11:02:58 +0900

    add debug overlay toggle

--- components/debug-overlay.tsx
+++ components/debug-overlay.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useDisclosure } from "@/hooks/use-disclosure";
+import { useHotkeys } from "@/hooks/use-hot-keys";
 import { useData } from "@/providers/data";
 import { memo, type FC } from "react";
 
@@ -7,7 +9,16 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const DebugOverlay: FC = () => {
   const { profile, platinums, groups } = useData();
+  const [visible, { toggle }] = useDisclosure();
+
+  useHotkeys([
+    ["mod+K", toggle],
+    ["ctrl+K", toggle],
+  ]);
+
   if (isProduction) return null;
+  if (!visible) return null;
+
   return (
     <>
       <div className="w-0 xl:w-[250px] absolute inset-y-1/2 -translate-y-1/2 left-3 overflow-auto h-[90%] flex flex-col justify-center items-center">
