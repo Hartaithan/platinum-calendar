ef6560ee48825578172ed8d2a31eef692bd5aa9d
Hartaithan <hartaithan@gmail.com>
2024-09-13 11:04:33 +0900

    add fetch target descriptions

--- components/settings-form.tsx
+++ components/settings-form.tsx
@@ -9,6 +9,14 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useSettings } from "@/providers/settings";
+import type { FetchTarget } from "@/models/fetch";
+
+const fetchTargetDescription: Record<FetchTarget, string> = {
+  alpha:
+    "more stable, but there may be issues retrieving profiles with over 1000+ platinums",
+  bravo:
+    "slower, but has no limitations. should only be used if you encounter issues with Alpha",
+};
 
 const SettingsForm: FC = () => {
   const { settings, handleTargetChange } = useSettings();
@@ -24,6 +32,9 @@ const SettingsForm: FC = () => {
           <SelectItem value="bravo">Bravo</SelectItem>
         </SelectContent>
       </Select>
+      <p className="text-xs text-left text-neutral-500 mt-2">
+        {fetchTargetDescription[settings.target]}
+      </p>
     </div>
   );
 };
