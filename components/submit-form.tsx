51fb1d1fc65c351cc277b2aae9f5072828c7c2da
Hartaithan <hartaithan@gmail.com>
2024-09-20 10:57:57 +0900

    disable form when data is loading

--- components/submit-form.tsx
+++ components/submit-form.tsx
@@ -2,11 +2,15 @@ import type { ComponentPropsWithoutRef, FC } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { SendHorizontalIcon } from "lucide-react";
+import { useData } from "@/providers/data";
 
 type Props = ComponentPropsWithoutRef<"form">;
 
 const SubmitForm: FC<Props> = (props) => {
   const { onSubmit } = props;
+  const { status } = useData();
+  const isLoading =
+    status === "platinums-loading" || status === "profile-loading";
   return (
     <form
       className="relative flex w-full max-w-sm items-center space-x-2"
@@ -15,11 +19,13 @@ const SubmitForm: FC<Props> = (props) => {
         name="id"
         className="w-full lg:w-96 h-9 pr-8 bg-secondary"
         placeholder="Enter your PSN ID"
+        disabled={isLoading}
       />
       <Button
         unstyled
         className="h-full absolute right-0 !ml-0 hover:opacity-80 px-2"
-        type="submit">
+        type="submit"
+        disabled={isLoading}>
         <SendHorizontalIcon className="size-5 stroke-1" />
       </Button>
     </form>
