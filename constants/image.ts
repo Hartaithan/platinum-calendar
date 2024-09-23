87d30aa07eb23c97dbf3e2b7b40faafc0844aefc
Hartaithan <hartaithan@gmail.com>
2024-09-23 10:51:30 +0900

    increase pixel ratio for generated image

--- constants/image.ts
+++ constants/image.ts
@@ -1,7 +1,7 @@
 import type { Options } from "html-to-image/lib/types";
 
 export const imageOptions: Options = {
-  pixelRatio: 1,
+  pixelRatio: 2,
   quality: 1,
   type: "image/png",
   cacheBust: true,
