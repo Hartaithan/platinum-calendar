deb17f43e67d91bed501d74085b8bd7705ae33fc
Hartaithan <hartaithan@gmail.com>
2024-10-06 14:31:42 +0900

    add responsive layout for heatmap calendar

--- components/heatmap-calendar.tsx
+++ components/heatmap-calendar.tsx
@@ -18,6 +18,8 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 interface MonthProps extends BaseMonthProps {
   onDayClick: DayClickHandler;
@@ -61,15 +63,25 @@ const Day: FC<DayProps> = memo((props) => {
   const platinums = groups ? groups[key] : null;
   const count = platinums?.length || 0;
   const hasPlatinums = !!platinums && platinums.length > 0;
+  const isTouchDevice = useMediaQuery("(pointer: coarse)");
+  const dayStyles = cn(styles.day, getDayColor(count));
 
-  if (!hasPlatinums) {
-    return <div className={cn(styles.day, getDayColor(count))} />;
+  if (!hasPlatinums) return <div className={dayStyles} />;
+
+  if (isTouchDevice) {
+    return (
+      <Button
+        unstyled
+        className={dayStyles}
+        onClick={() => onDayClick({ date, platinums })}
+      />
+    );
   }
 
   return (
     <Tooltip delayDuration={100}>
       <TooltipTrigger
-        className={cn(styles.day, getDayColor(count))}
+        className={dayStyles}
         onClick={() => onDayClick({ date, platinums })}
       />
       <TooltipContent>
@@ -84,9 +96,9 @@ const Month: FC<MonthProps> = memo((props) => {
   const count = monthLength[month];
   const days = createArray(count);
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-fit justify-self-center">
       <h3 className="font-semibold mb-2">{monthLabels[month].long}</h3>
-      <div className="grid grid-cols-7 gap-1">
+      <div className="grid grid-cols-7 gap-1.5">
         {days.map((day) => (
           <Day
             key={`month-${day}`}
@@ -102,10 +114,12 @@ const Month: FC<MonthProps> = memo((props) => {
 
 const Legend: FC = () => {
   return (
-    <div className="w-full flex items-end">
-      <div className="mt-6 flex items-center justify-end">
-        <span className="text-sm mr-2">Less</span>
-        <div className="flex gap-1">
+    <div className="w-full flex justify-center lg:justify-end @save:justify-end">
+      <div className="mt-6 grid grid-cols-[1fr_1fr] md:grid-cols-[1fr_auto_1fr] @save:grid-cols-[1fr_auto_1fr] gap-y-1 md:gap-y-0 @save:gap-y-0">
+        <span className="text-sm mr-2 row-[1/2] md:row-auto @save:row-auto col-[1/2] md:col-auto @save:col-auto text-left">
+          Less
+        </span>
+        <div className="flex gap-1 row-[2/3] md:row-auto @save:row-auto col-[1/3] md:col-auto @save:col-auto">
           {dayColors.map(([bg, fg], index) => (
             <div
               key={`legend-${index}`}
@@ -116,7 +130,9 @@ const Legend: FC = () => {
             </div>
           ))}
         </div>
-        <span className="text-sm ml-2">More</span>
+        <span className="text-sm ml-2 row-[1/2] md:row-auto @save:row-auto col-[2/3] md:col-auto @save:col-auto text-right">
+          More
+        </span>
       </div>
     </div>
   );
@@ -126,7 +142,7 @@ const HeatMapCalendar: FC<CalendarProps> = (props) => {
   const { onDayClick } = props;
   return (
     <div className="flex flex-col flex-1 justify-center">
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 @save:grid-cols-4 gap-4">
         {monthIndex.map((month) => (
           <Month key={month} month={month} onDayClick={onDayClick} />
         ))}
