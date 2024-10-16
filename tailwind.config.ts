de6fb9770028669a0bb9e89bd80b0972a94aba11
Hartaithan <hartaithan@gmail.com>
2024-10-16 14:37:05 +0900

    install accordion component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,6 +71,20 @@ const config: Config = {
       size: {
         day: "33px",
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [containerQueries, tailwindAnimate],
