7d6458fed907b439f6596215dc28435ffc9e64fa
Hartaithan <hartaithan@gmail.com>
2025-03-14 21:29:54 +0900

    custom export image size for each theme

--- providers/capture.tsx
+++ providers/capture.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import type { Theme } from "@/models/app";
 import { useTheme } from "@/providers/theme";
 import { captureElement } from "@/utils/capture";
 import { readError } from "@/utils/error";
@@ -27,11 +28,17 @@ const initialValue: Context = {
 
 const Context = createContext<Context>(initialValue);
 
+const sizes: Record<Theme, { width: number; height: number }> = {
+  heatmap: { width: 1200, height: 800 },
+  og: { width: 1200, height: 800 },
+};
+
 const CaptureProvider: FC<PropsWithChildren> = (props) => {
   const { children } = props;
   const captureRef = useRef<HTMLDivElement>(null);
   const tempRef = useRef<HTMLDivElement>(null);
   const { theme } = useTheme();
+  const { width, height } = sizes[theme];
 
   const capture = useCallback(async (): Promise<Blob | null> => {
     const calendar = captureRef.current;
@@ -42,13 +49,14 @@ const CaptureProvider: FC<PropsWithChildren> = (props) => {
       hidden.appendChild(calendar.cloneNode(true));
       const image = await captureElement(hidden, theme);
       if (!image) throw new Error("Unable to generate image");
-      hidden.innerHTML = "";
       return image;
     } catch (error) {
       console.error("generate image error", error);
       const message = readError(error);
       toast.error(message);
       return null;
+    } finally {
+      hidden.innerHTML = "";
     }
   }, [theme]);
 
@@ -62,8 +70,9 @@ const CaptureProvider: FC<PropsWithChildren> = (props) => {
       {children}
       <div className="fixed -left-full top-0 -z-50 h-full w-full overflow-hidden">
         <div
-          className="flex h-[800px] w-[1200px] flex-col @container"
+          className="flex flex-col @container"
           ref={tempRef}
+          style={{ width, height }}
         />
       </div>
     </Context.Provider>
