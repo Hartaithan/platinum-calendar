68aa8b983067561c55d5589cb3d018a0c0d029bd
Hartaithan <hartaithan@gmail.com>
2025-03-21 16:58:06 +0900

    remove debug overlay component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import DebugOverlay from "@/components/debug-overlay";
 import MainSection from "@/components/main-section";
 import type { FC } from "react";
 
 const HomePage: FC = () => (
   <main className="flex flex-1 items-center justify-center">
     <MainSection />
-    <DebugOverlay />
   </main>
 );
 
