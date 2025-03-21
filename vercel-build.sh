c860aa4237f0c5f85e55801ff2b7f78ce0d7c917
Hartaithan <hartaithan@gmail.com>
2025-03-21 21:56:28 +0900

    update vercel build command

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "$VERCEL_GIT_COMMIT_REF" = "prod" ] || [ "$VERCEL_GIT_COMMIT_REF" = "preview" ]; then
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "prod" || "$VERCEL_GIT_COMMIT_REF" == "preview" ]]; then
   echo "build can proceed"
   exit 1;
 else
-  echo "build ignored"
-  exit 0; 
+  echo "build cancelled"
+  exit 0;
 fi
