33595adc26447084ac8d12ee78650e55e5b7a060
Hartaithan <hartaithan@gmail.com>
2025-03-25 17:14:28 +0900

    add vercel env logging in build command

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_ENV: $VERCEL_ENV"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "prod" || "$VERCEL_GIT_COMMIT_REF" == "preview" ]]; then
   echo "build can proceed"
